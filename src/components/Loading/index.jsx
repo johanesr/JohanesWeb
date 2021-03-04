d88a5a0351121429a26c30b4ec978362db4acd67
ronaldo97 <johanesronaldo@gmail.com>
2021-03-04 21:53:12 +0700

    Loading Dark Theme

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -2,7 +2,7 @@ import './styles.scss';
 
 function Loading(props) {
   return(
-    <div className={props.theme ? "loading" : "loading dark"}>
+    <div className={props.curTheme ? "loading" : "loading dark"}>
       <div className="loading-text">
         Please wait while we render the pages :(
       </div>
